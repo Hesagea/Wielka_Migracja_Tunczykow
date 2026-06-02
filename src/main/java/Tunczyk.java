8e226ab9d08daa15bccb9d9d1901041106b7afe5
Bobrbezfutra <ciesielskahanna839@gmail.com>
2026-06-02 15:25:54 +0200

    TUNNNNCZYYYYYKKK

--- src/main/java/Tunczyk.java
+++ src/main/java/Tunczyk.java
@@ -1,14 +1,28 @@
-public class Tunczyk {
-    private int id, x, y;
-    private double energy;
+public class Tunczyk extends Agent {
 
-    // z jakiego iejsca startują tunczyki, czy energia jest taka sama na start czy nie????????????
-    public Tunczyk(int id, int y, double energy){
-        this.id=id;
-        this.x=0;
-        this.y=y;
-        this.energy=energy;
+    private StrategiaRuchu mojaStrategia;
+
+    // 1. Konstruktor
+    public Tunczyk(int id, int x, int y) {
+        // "super" przekazuje dane (id, x, y) w górę, do konstruktora Agenta
+        super(id, x, y);
+
+        this.mojaStrategia = new RuchLosowy();
     }
 
+    @Override
+    public void krok() {
+        Wektor2D wektor = mojaStrategia.wyliczWektorRuchu();
 
+        int potencjalneX = this.x + (int) wektor.getDx();
+        int potencjalneY = this.y + (int) wektor.getDy();
+
+        Komorka bezpiecznaKomorka = Symulacja.getInstance().getPlansza().getKomorka(potencjalneX, potencjalneY);
+
+        // Przypisujemy rybie ostateczne, bezpieczne współrzędne pobrane z Torusa
+        this.x = bezpiecznaKomorka.getX();
+        this.y = bezpiecznaKomorka.getY();
+        // zeby było cos widac
+        System.out.println("Tuńczyk nr " + id + " przepłynął na nowe współrzędne: (" + this.x + ", " + this.y + ")");
+    }
 }
