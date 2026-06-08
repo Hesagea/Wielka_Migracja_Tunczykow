60da7c3ac4d402ea9b788aa995725a400d143352
Bobrbezfutra <ciesielskahanna839@gmail.com>
2026-06-08 12:12:10 +0200

    analiza tunczykow

--- src/main/java/RuchLosowy.java
+++ src/main/java/RuchLosowy.java
@@ -4,11 +4,13 @@ public class RuchLosowy implements StrategiaRuchu {
 
     private Random random = new Random();
 
+    @Override
     public Wektor2D wyliczWektorRuchu() {
+        // Losowy wektor dx od 5 do 10 (zawsze do przodu ku tarlisku)
         double dx = random.nextInt(6) + 5;
+        // Losowy wektor dy od -5 do 5 (zygzak góra/dół)
         double dy = random.nextInt(11) - 5;
 
-        // Zwracamy gotowy wektor wyliczony przez strategię
         return new Wektor2D(dx, dy);
     }
 }
--- src/main/java/StrategiaRuchu.java
+++ src/main/java/StrategiaRuchu.java
@@ -1,21 +1,5 @@
-import java.util.List;
+public interface StrategiaRuchu {
 
-public class StrategiaStadna implements StrategiaRuchu {
 
-    // Strategia
-    @Override
-    public Wektor2D wyliczWektorRuchu() {
-        List<Tunczyk> wszystkieRyby = Symulacja.getInstance().getListaTunczykow();
-
-        double bazaDx = 1.0;
-        double bazaDy = 0.0;
-        double separacjaX = 0.0, separacjaY = 0.0;
-
-        for (Tunczyk inna : wszystkieRyby) {
-            // Logika Boids: Wyliczanie odległości i unikanie nakładania się ryb
-            // Tuńczyk pobierze ten wektor i zmieni pozycję
-        }
-
-        return new Wektor2D(bazaDx + separacjaX, bazaDy + separacjaY);
-    }
+    Wektor2D wyliczWektorRuchu();
 }
